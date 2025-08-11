3efa43c4ac385e9da24e1fd76fcce4b890b067e7
RishitP <rishitprabhu.personal@gmail.com>
2025-08-11 19:47:42 +0530

    Added Collision for Ball

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ const int windowWidth = 800;
 
 
 Vector2 ballPosition = {(float)windowWidth/2, (float)windowHeight/2};
+Vector2 ballVelocity = {2, 2};
 Vector2 p1Position = {(float)0, (float)windowHeight/2  - 30};
 Vector2 p2Position = {(float)windowWidth - 10, (float)windowHeight/2  - 30};
 
@@ -36,13 +37,43 @@ int p2Input(float posY){
   return posY;
 }
 
+Vector2 CollisionCheck(Vector2 pos, Vector2 vel){
+  if(pos.y >= windowHeight - 15/2)
+  {
+    vel.y =  -(vel.y);
+  };
+  if(pos.y <= 15/2)
+  {
+    vel.y =  -(vel.y);
+  };
+
+  if(pos.x >= windowWidth - 15/2)
+  {
+    vel.x =  -(vel.x);
+  };
+  if(pos.x <= 15/2)
+  {
+    vel.x =  -(vel.x);
+  };
+  return vel;
+}
+
 int main() {
 
   InitWindow(windowWidth, windowHeight, "Ping Pong");
   SetTargetFPS(120);
 
+  float dt = 0;
+
   while(!WindowShouldClose())
   {
+    dt = GetFrameTime();
+
+    ballVelocity = CollisionCheck(ballPosition, ballVelocity);
+
+    ballPosition.x +=  ballVelocity.x;
+    ballPosition.y +=  ballVelocity.y;
+
     BeginDrawing();
       ClearBackground(BACKGROUND_GREEN);
       DrawText("Let's Play Ping Pong", windowWidth/2 - 100,  windowHeight/2 + 50, 20, OFFWHITE);
