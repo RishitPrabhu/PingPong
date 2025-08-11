25f3e7534ed4433272ff49bde4fd3d88b8fb4d0d
RishitP <rishitprabhu.personal@gmail.com>
2025-08-11 19:25:58 +0530

    Added Player Movement Functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,31 @@ const int windowWidth = 800;
 
 
 Vector2 ballPosition = {(float)windowWidth/2, (float)windowHeight/2};
-Vector2 p1Position = {(float)0, (float)windowHeight/2  - 15};
+Vector2 p1Position = {(float)0, (float)windowHeight/2  - 30};
+Vector2 p2Position = {(float)windowWidth - 10, (float)windowHeight/2  - 30};
 
-int Input(float posY){
-  if(IsKeyDown(KEY_DOWN))
-    posY += 2;
-  if(IsKeyDown(KEY_UP))
-    posY -= 2;
+int p1Input(float posY){
+  if(posY >= 5){
+    if(IsKeyDown(KEY_W))
+      posY -= 2;
+  }
+  if(posY <= windowHeight - 60){
+    if(IsKeyDown(KEY_S))
+      posY += 2;
+  }
+    
+  return posY;
+}
+
+int p2Input(float posY){
+  if(posY >= 5){
+    if(IsKeyDown(KEY_UP))
+      posY -= 2;
+  }
+  if(posY <= windowHeight - 60){
+    if(IsKeyDown(KEY_DOWN))
+      posY += 2;
+  }
   return posY;
 }
 
@@ -29,11 +47,12 @@ int main() {
       ClearBackground(BACKGROUND_GREEN);
       DrawText("Let's Play Ping Pong", windowWidth/2 - 100,  windowHeight/2 + 50, 20, OFFWHITE);
 
-      DrawCircleV(ballPosition, 20, OFFWHITE);
+      DrawCircleV(ballPosition, 15, OFFWHITE);
 
-      p1Position.y = Input(p1Position.y);
-      DrawRectangleV(p1Position, (Vector2){10, 30}, OFFWHITE);
-      
+      p1Position.y = p1Input(p1Position.y);
+      p2Position.y = p2Input(p2Position.y);
+      DrawRectangleV(p1Position, (Vector2){10, 60}, OFFWHITE);
+      DrawRectangleV(p2Position, (Vector2){10, 60}, OFFWHITE);
 
     EndDrawing();
   }
