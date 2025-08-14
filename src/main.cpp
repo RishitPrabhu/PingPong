3230d5e6be5089a3146b9447d2e51257d3627a9d
RishitP <rishitprabhu.personal@gmail.com>
2025-08-15 00:29:43 +0530

    Added Functionalities

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <vector>
 
-#define BACKGROUND_GREEN CLITERAL (Color) {166, 178, 139, 255}
-#define OFFWHITE CLITERAL (Color){249, 246, 243, 255}
+using namespace std;
+
+#define BACKGROUND_GREEN CLITERAL(Color) {166, 178, 139, 255}
+#define OFFWHITE CLITERAL(Color){249, 246, 243, 255}
 
 const int windowHeight = 450;
 const int windowWidth = 800;
@@ -10,11 +13,10 @@ const int windowWidth = 800;
 Vector2 ballPosition = {(float)windowWidth/2, (float)windowHeight/2};
 Vector2 ballVelocity = {2, 2};
 Vector2 p1Position = {(float)0, (float)windowHeight/2  - 30};
-float p1Velocity = 0;
-float p2Velocity = 0;
 Vector2 p2Position = {(float)windowWidth - 10, (float)windowHeight/2  - 30};
+vector<int> score = {0, 0};
 
-int p1Input(float posY){
+float p1Input(float posY){
   if(posY >= 5){
     if(IsKeyDown(KEY_W))
       posY -= 2;
@@ -27,7 +29,7 @@ int p1Input(float posY){
   return posY;
 }
 
-int p2Input(float posY){
+float p2Input(float posY){
   if(posY >= 5){
     if(IsKeyDown(KEY_UP))
       posY -= 2;
@@ -39,17 +41,27 @@ int p2Input(float posY){
   return posY;
 }
 
+Vector2 sideCollision(Vector2 pos)
+{
+  if(pos.x >= windowWidth - 15.0/2)
+    {
+      score[0]++;
+      pos = {windowWidth/2, windowHeight/2};
+    }
+  if(pos.x <= 15.0/2)
+    {
+      score[1]++;
+      pos = {windowWidth/2, windowHeight/2};
+    }
+    return pos;
+}
+
 Vector2 CollisionCheck(Vector2 pos, Vector2 vel){
   if(pos.y >= windowHeight - 15.0/2)
     vel.y =  -(vel.y);
   if(pos.y <= 15.0/2)
     vel.y =  -(vel.y);
-  if(pos.x >= windowWidth - 15.0/2)
-      vel.x =  -(vel.x);
-  if(pos.x <= 15.0/2)
-      vel.x =  -(vel.x);
   
-  //if(pos.x <= p1Position.x);
 
   return vel;
 }
@@ -57,9 +69,9 @@ Vector2 CollisionCheck(Vector2 pos, Vector2 vel){
 Vector2 p1CollisionCheck(Vector2 p1Position, Vector2 pos, Vector2 vel){
   if(pos.x <=  12 + (15.0/2))
   {
-    if(pos.y >= p1Position.y && pos.y <= p1Position.y + 60){
+    if(pos.y >= p1Position.y && pos.y <= p1Position.y + 60)
+    {
       vel.x = -(vel.x);
-      vel.y = -(vel.y);
     }
   }
   return vel;
@@ -70,7 +82,6 @@ Vector2 p2CollisionCheck(Vector2 p2Position, Vector2 pos, Vector2 vel){
   {
     if(pos.y >= p2Position.y && pos.y <= p2Position.y + 60){
       vel.x = -(vel.x);
-      vel.y = -(vel.y);
     }
   }
   return vel;
@@ -90,19 +101,20 @@ int main() {
     ballVelocity = CollisionCheck(ballPosition, ballVelocity);
     ballVelocity = p1CollisionCheck(p1Position, ballPosition, ballVelocity);
     ballVelocity = p2CollisionCheck(p2Position, ballPosition, ballVelocity);
-
     ballPosition = Vector2Add(ballPosition, ballVelocity);
+    ballPosition = sideCollision(ballPosition);
 
     BeginDrawing();
       ClearBackground(BACKGROUND_GREEN);
       DrawText("Let's Play Ping Pong", windowWidth/2 - 100,  windowHeight/2 + 50, 20, OFFWHITE);
-      
+      DrawText(TextFormat("%03i", score[0]), windowWidth/2 - 100,  50, 20, OFFWHITE);
+      DrawText(TextFormat("%03i", score[1]), windowWidth/2 + 100,  50, 20, OFFWHITE);
       DrawCircleV(ballPosition, 15, OFFWHITE);
 
       p1Position.y = p1Input(p1Position.y);
       p2Position.y = p2Input(p2Position.y);
       DrawRectangleV(p1Position, (Vector2){10, 60}, OFFWHITE);
-      DrawRectangleV(p2Position, (Vector2){1, 60}, OFFWHITE);
+      DrawRectangleV(p2Position, (Vector2){10, 60}, OFFWHITE);
 
     EndDrawing();
   }
